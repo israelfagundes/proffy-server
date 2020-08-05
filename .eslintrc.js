234e55b40af039fdda178f806ea43860b71cccc2
Israel <israel.fagundes2001@gmail.com>
2020-08-05 15:49:39 -0300

    update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,36 @@ module.exports = {
     es2020: true,
     node: true,
   },
-  extends: ['airbnb-base', 'prettier'],
+  extends: [
+    'airbnb-base',
+    'prettier',
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 11,
+    ecmaVersion: 2020,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint', 'prettier'],
-  rules: {},
+  rules: {
+    'class-methods-use-this': 'off',
+    'no-unused-vars': 'off',
+    'prettier/prettier': 'error',
+    'import/extensions': [
+      'off',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    camelcase: 'off',
+    'import/no-unresolved': 'off',
+    'func-names': 'off',
+  },
 };
